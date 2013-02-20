7822be0d8eed5c2d78238e9a84f587245fe63352
David Souther <davidsouther@gmail.com>
2013-02-19 21:57:58 -0500

    Fixed for JSHint

--- src/canvas/scripts/d3.layout.spiral.js
+++ src/canvas/scripts/d3.layout.spiral.js
@@ -1,29 +1,29 @@
 d3.layout.spiral = function(options){
 	var settings = {
 		exponent: options.exponent || 1/2,
-		spins: options.spins || 3,
+		spins: options.spins || 3
 	};
 
 	settings.func = options.func || d3.layout.spiral.archimedes();
 
 	function Ticker(points){
 		var theta = 0,
-		    scale = d3.scale.pow()
-		                    .domain([0, points])
-		                    .range([0, settings.spins * 2 * Math.PI])
-		                    .exponent(settings.exponent);
+			scale = d3.scale.pow()
+				.domain([0, points])
+				.range([0, settings.spins * 2 * Math.PI])
+				.exponent(settings.exponent);
 
 		return function(){
 			return scale(theta++);
-		}
+		};
 	}
 
 	// Nodes are objects that will get `px, py` set relative to the spiral's center.
 	// Follows an Archimedian spiral `r = a + b * theta`
 	var spiral = function(nodes){
 		var ticker = Ticker(nodes.length),
-		    r, theta, node,
-		    i_, len_ = nodes.length;
+			r, theta, node,
+			i_, len_ = nodes.length;
 
 		for(i_ = 0; i_ < len_; i_++){
 			node = nodes[i_];
@@ -44,7 +44,7 @@ d3.layout.spiral = function(options){
 		if(spins === null){ return settings.spin; }
 		settings.spins = spins;
 		return this;
-	}
+	};
 
 	return spiral;
 };
@@ -54,7 +54,7 @@ d3.layout.spiral.linear = d3.layout.spiral.archimedes = function(a, b){
 	b = b || 1;
 	return function(theta){
 		return a + (b * theta);
-	}
+	};
 };
 
 d3.layout.spiral.log = function(a, b){
@@ -62,9 +62,9 @@ d3.layout.spiral.log = function(a, b){
 	b = b || 1;
 	return function(theta){
 		return a * Math.exp(b * theta);
-	}
+	};
 };
 
 d3.layout.spiral.golden = function(a){
 	return d3.layout.spiral.log(a, 0.306349);
-}
+};
